870ceb112dc502e615b27ad8025e697e9fab3e92
Chandan Suri <csuri21296@gmail.com>
2022-06-12 18:09:27 -0400

    Added code for Product Sum program

--- Recursion/Easy/Product Sum.py
+++ Recursion/Easy/Product Sum.py
@@ -1,3 +1,14 @@
 # Tip: You can use the type(element) to check whether an item is a list or an integer
 def productSum(array):
-    pass
+    return productSumHelper(array, 1)
+
+
+def productSumHelper(array, depth):
+    currSum = 0
+    for element in array:
+        if type(element) is list:
+            currSum += productSumHelper(element, depth + 1)
+        else:
+            currSum += element
+
+    return currSum * depth
