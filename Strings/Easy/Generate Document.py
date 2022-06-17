0e4581612238326f800430646c9ba5a402415517
Chandan Suri <csuri21296@gmail.com>
2022-06-17 19:49:33 -0400

    Added code for generating document (map based string program).

--- Strings/Easy/Generate Document.py
+++ Strings/Easy/Generate Document.py
@@ -1,3 +1,15 @@
 def generateDocument(characters, document):
-    # Write your code here.
-    return False
+    if len(document) == 0:
+        return True
+
+    charMap = dict()
+    for char in characters:
+        charMap[char] = charMap.get(char, 0) + 1
+
+    for char in document:
+        if char not in charMap or charMap[char] == 0:
+            return False
+
+        charMap[char] -= 1
+
+    return True
