2d95c8f5e582e152540099e37e1d88114b7679d2
Chandan Suri <csuri21296@gmail.com>
2022-06-16 21:39:39 -0400

    Added code for Caesar Cypher Encryptor program.

--- Strings/Easy/Caesar Cypher Encryptor.py
+++ Strings/Easy/Caesar Cypher Encryptor.py
@@ -1,3 +1,11 @@
 def caesarCipherEncryptor(string, key):
-    strSize = len(string)
-    characters = ""
+    encryptedStr = list()
+    alphabets = "abcdefghijklmnopqrstuvwxyz"
+    key = key % len(alphabets)
+
+    for alphabet in string:
+        idx = alphabets.index(alphabet)
+        newCharIdx = (idx + key) % len(alphabets)
+        encryptedStr.append(alphabets[newCharIdx])
+
+    return "".join(encryptedStr)
