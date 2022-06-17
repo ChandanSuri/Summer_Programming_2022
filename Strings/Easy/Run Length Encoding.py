ca2d2f6be2bbd485e0ecd569a99baf394831392e
Chandan Suri <csuri21296@gmail.com>
2022-06-17 19:49:09 -0400

    Added code for Run Length Encoding - Compression based program.

--- Strings/Easy/Run Length Encoding.py
+++ Strings/Easy/Run Length Encoding.py
@@ -1,3 +1,20 @@
 def runLengthEncoding(string):
-    # Write your code here.
-    pass
+    encodedStr = list()
+    counter = 1
+    lenStr = len(string)
+
+    for idx in range(1, lenStr):
+        currChar = string[idx]
+        prevChar = string[idx - 1]
+
+        if currChar != prevChar or counter == 9:
+            encodedStr.append(str(counter))
+            encodedStr.append(prevChar)
+            counter = 0
+
+        counter += 1
+
+    encodedStr.append(str(counter))
+    encodedStr.append(string[-1])
+
+    return "".join(encodedStr)
