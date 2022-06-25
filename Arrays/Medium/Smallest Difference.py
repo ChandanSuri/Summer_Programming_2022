cfe421e868ec4113b89980d66073e99cc835e470
Chandan Suri <csuri21296@gmail.com>
2022-06-25 12:39:30 -0400

    Added code for finding smallest difference pair in the array

--- Arrays/Medium/Smallest Difference.py
+++ Arrays/Medium/Smallest Difference.py
@@ -1,3 +1,23 @@
 def smallestDifference(arrayOne, arrayTwo):
-    # Write your code here.
-    pass
+    arrayOnePtr = arrayTwoPtr = 0
+    minDiff = float("inf")
+    result = [float("inf"), float("inf")]
+
+    while arrayOnePtr < len(arrayOne) and arrayTwoPtr < len(arrayTwo):
+        arrayOneEle = arrayOne[arrayOnePtr]
+        arrayTwoEle = arrayTwo[arrayTwoPtr]
+        currDiff = abs(arrayOneEle - arrayTwoEle)
+
+        if currDiff < minDiff:
+            minDiff = currDiff
+            result = [arrayOneEle, arrayTwoEle]
+
+            if minDiff == 0:
+                return result
+
+        if arrayOneEle < arrayTwoEle:
+            arrayOnePtr += 1
+        else:
+            arrayTwoPtr += 1
+
+    return result
