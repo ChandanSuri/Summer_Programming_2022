00edc5192a19fc46b786e22d73ac97bca27d10d6
Chandan Suri <csuri21296@gmail.com>
2022-06-26 17:05:00 -0400

    added code for longest Peak program and some comments

--- Arrays/Medium/Longest Peak.py
+++ Arrays/Medium/Longest Peak.py
@@ -1,3 +1,29 @@
+# O(N) time and O(1) space
 def longestPeak(array):
-    # Write your code here.
-    pass
+    idx = 1
+    maxPeakSize = 0
+
+    while idx < len(array) - 1:
+        prevElem = array[idx - 1]
+        currElem = array[idx]
+        nextElem = array[idx + 1]
+
+        isPeak = prevElem < currElem and currElem > nextElem
+        if not isPeak:
+            idx += 1
+            continue
+
+        leftIdx = idx - 1
+        while leftIdx >= 0 and array[leftIdx] < array[leftIdx + 1]:
+            leftIdx -= 1
+
+        rightIdx = idx + 1
+        while rightIdx < len(array) and array[rightIdx] < array[rightIdx - 1]:
+            rightIdx += 1
+
+        currPeakSize = rightIdx - leftIdx - 1
+        maxPeakSize = max(currPeakSize, maxPeakSize)
+
+        idx = rightIdx - 1
+
+    return maxPeakSize
