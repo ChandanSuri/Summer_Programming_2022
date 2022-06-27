90efb025db87062118cf651d63995e5c4a4e9044
Chandan Suri <csuri21296@gmail.com>
2022-06-26 21:49:29 -0400

    added code for merging overlapping intervals in O(nlogn) time and O(n) space

--- Arrays/Medium/Merge Overlapping Intervals.py
+++ Arrays/Medium/Merge Overlapping Intervals.py
@@ -1,3 +1,17 @@
 def mergeOverlappingIntervals(intervals):
-    # Write your code here.
-    return [[]]
+    intervals = sorted(intervals, key=lambda ele: ele[0])
+    currInterval = intervals[0]
+    mergedIntervals = list()
+
+    for idx in range(1, len(intervals)):
+        nextInterval = intervals[idx]
+
+        if currInterval[1] >= nextInterval[0]:
+            currInterval[1] = max(currInterval[1], nextInterval[1])
+        else:
+            mergedIntervals.append(currInterval)
+            currInterval = nextInterval
+
+    mergedIntervals.append(currInterval)
+
+    return mergedIntervals
