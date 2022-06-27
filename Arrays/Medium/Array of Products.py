7451d56b5bfcaf86df1e765001fe083d0d5ea148
Chandan Suri <csuri21296@gmail.com>
2022-06-26 20:27:14 -0400

    added code for Array of Products except self program

--- Arrays/Medium/Array of Products.py
+++ Arrays/Medium/Array of Products.py
@@ -1,3 +1,14 @@
 def arrayOfProducts(array):
-    # Write your code here.
-    pass
+    products = [1] * len(array)
+
+    leftRunningProduct = 1
+    for idx in range(len(array)):
+        products[idx] = leftRunningProduct
+        leftRunningProduct *= array[idx]
+
+    rightRunningProduct = 1
+    for idx in range(len(array) - 1, -1, -1):
+        products[idx] *= rightRunningProduct
+        rightRunningProduct *= array[idx]
+
+    return products
