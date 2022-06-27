1a7f7ee81f87cd9188c4e4765061e8a507022bc1
Chandan Suri <csuri21296@gmail.com>
2022-06-26 21:09:07 -0400

    added code for finding first duplicate value in constant space when numbers are in the range 1 to n itself.

--- Arrays/Medium/First Duplicate Value.py
+++ Arrays/Medium/First Duplicate Value.py
@@ -1,3 +1,11 @@
 def firstDuplicateValue(array):
-    # Write your code here.
-    return []
+    idx = 0
+    while idx < len(array):
+        currValue = array[idx]
+        nextIdx = abs(currValue) - 1
+        if array[nextIdx] < 0:
+            return abs(currValue)
+        array[nextIdx] *= -1
+        idx += 1
+
+    return -1
