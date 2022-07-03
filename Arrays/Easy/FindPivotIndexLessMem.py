823b7fd2b89416037d51a49bc5acd5889a8b993b
Chandan Suri <csuri21296@gmail.com>
2022-07-03 03:36:29 -0400

    Added code for finding the pivot index using less memory (constant space) (leetcode)

--- Arrays/Easy/FindPivotIndexLessMem.py
+++ Arrays/Easy/FindPivotIndexLessMem.py
@@ -5,4 +5,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        pass
+
+        total_sum = sum(nums)
+        left_running_sum = 0
+        for idx, num in enumerate(nums):
+            if left_running_sum == total_sum - left_running_sum - num:
+                return idx
+            left_running_sum += num
+
+        return -1
