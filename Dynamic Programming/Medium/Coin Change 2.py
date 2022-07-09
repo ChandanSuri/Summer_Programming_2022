9ddb6e8a32b002f513a290f5bdb9a6f3255c800a
Chandan Suri <csuri21296@gmail.com>
2022-07-09 08:40:43 -0400

    Added code for create the change of amount with the denominations (infinite coins) provided.

--- Dynamic Programming/Medium/Coin Change 2.py
+++ Dynamic Programming/Medium/Coin Change 2.py
@@ -5,3 +5,12 @@ class Solution(object):
         :type coins: List[int]
         :rtype: int
         """
+        numCoins = len(coins)
+        numWaysForAmounts = [0] * (amount + 1)
+        numWaysForAmounts[0] = 1
+
+        for coin in coins:
+            for currAmount in range(coin, amount + 1):
+                numWaysForAmounts[currAmount] += numWaysForAmounts[currAmount - coin]
+
+        return numWaysForAmounts[-1]
