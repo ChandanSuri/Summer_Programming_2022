0fc9bcc90ee1320e019bd96e47b71e5d86e3f5b3
Chandan Suri <csuri21296@gmail.com>
2022-07-11 00:40:38 -0400

    Added code for removing duplicates from a string.

--- Strings/Medium/Remove all adjacent duplicates in a String.py
+++ Strings/Medium/Remove all adjacent duplicates in a String.py
@@ -1,7 +1,30 @@
 class Solution(object):
+    # This solution is using Naive approach with just using memoization for the counts of the each character
+    # and then reverting back to from where the deletion was done, to be sure that we don't miss cases
+    # and thus, don't have to repeatedly go over the same set of characters.
+    # Time: O(n) and Space: O(n) complexity.
     def removeDuplicates(self, s, k):
         """
         :type s: str
         :type k: int
         :rtype: str
         """
+        str_ls = list(s)
+        count_chars = [0] * len(s)
+        count_chars[0] = 1
+        idx = 1
+
+        while idx < len(str_ls):
+            prev_char = str_ls[idx - 1]
+            curr_char = str_ls[idx]
+            if curr_char != prev_char:
+                count_chars[idx] = 1
+            else:
+                count_chars[idx] = count_chars[idx - 1] + 1
+                if count_chars[idx] == k:
+                    del str_ls[idx - k + 1: idx + 1]
+                    idx = idx - k
+
+            idx += 1
+
+        return ''.join(str_ls)
