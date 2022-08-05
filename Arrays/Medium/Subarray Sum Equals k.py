893991ce68075ff86a637222ba38342157262f97
Chandan Suri <csuri21296@gmail.com>
2022-08-05 00:16:32 -0400

    Added code for sub array sum len that equals k value in linear time.

--- Arrays/Medium/Subarray Sum Equals k.py
+++ Arrays/Medium/Subarray Sum Equals k.py
@@ -5,3 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        sumsCountMap = dict()
+        lenNums = len(nums)
+        sumsCountMap[0] = 1
+        runningSum = 0
+        count = 0
+
+        for idx in range(lenNums):
+            runningSum += nums[idx]
+            if (runningSum - k) in sumsCountMap:
+                count += sumsCountMap[runningSum - k]
+            sumsCountMap[runningSum] = sumsCountMap.get(runningSum, 0) + 1
+
+        return count
